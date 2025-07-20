2452c06056322aace847185ba4f3b403e05620ac
kamil <kamil@MacBook-Air-kamil.local>
2025-07-20 12:12:33 +0200

    arrow dwon

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,7 @@
 
 import React from 'react';
 import ThreeCanvas from './ThreeCanvas';
+import { FaAnglesDown } from "react-icons/fa6";
 
 const Hero: React.FC = () => {
   return (
@@ -14,12 +15,21 @@ const Hero: React.FC = () => {
         <p className="max-w-2xl mx-auto text-lg md:text-xl text-slate-300 mb-8">
           I'm a dedicated full stack developer and IT engineering student passionate about creating robust web applications and innovative digital experiences.
         </p>
-        <a 
-          href="#projects" 
+        <a
+          href="#projects"
           className="bg-sky-500 text-white font-bold py-3 px-8 rounded-full text-lg hover:bg-sky-600 transition-transform duration-300 transform hover:scale-105"
         >
           View My Work
         </a>
+      </div>
+
+
+      <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 z-10">
+        <div className="flex flex-col items-center text-white hover:text-sky-400 transition-colors duration-300">
+          <div className="animate-bounce">
+            <FaAnglesDown className="w-8 h-8 font-xl" />
+          </div>
+        </div>
       </div>
     </section>
   );
